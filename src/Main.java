2f048156fad1639bd2abe26f29b70bcf4d5eb6e8
williampputra <wpputra90@gmail.com>
2022-12-17 21:19:01 +0700

    Contoh Implementasi Queue (Antrian)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args){
+        Antrian antrian = new Antrian(10);
+        antrian.masuk(13);
+        antrian.lihat();
+        antrian.masuk(32);
+        antrian.lihat();
+        antrian.masuk(45);
+        antrian.lihat();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
+        System.out.println("yang diambil dari antrian = " + antrian.keluar());
+        antrian.lihat();
     }
 }
